e2e99d3b6816dc042890f261b6543e3a391e73d2
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-07-09 14:53:50 +0700

    style: Refactor PomodoroView component to use Sidebar and Box for layout

--- src/views/tabs/PomodoroView.tsx
+++ src/views/tabs/PomodoroView.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { Box } from "@mui/material";
+import Sidebar from "../../components/Sidebar";
 
 const PomodoroView = () => {
-  return <div>PomodoroView</div>;
+  const PromodoroMain = () => {
+    return <Box></Box>;
+  };
+
+  return <Sidebar children={<PromodoroMain />} />;
 };
 
 export default PomodoroView;
